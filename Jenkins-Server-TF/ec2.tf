94b3208caa9fa453299c43e0f74156172aec15fe
Roopak911 <roopaktelharkkar@yahoo.com>
2024-05-20 15:33:16 +0530

    added role affiliation to jenkins server

--- Jenkins-Server-TF/ec2.tf
+++ Jenkins-Server-TF/ec2.tf
@@ -54,3 +54,29 @@ resource "aws_instance" "web" {
   }
 
 }
+
+
+resource "aws_iam_role" "iam-role" {
+  name               = "Jenkins-role"
+  assume_role_policy = <<-EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy_attachment" "iam-policy" {
+  role = aws_iam_role.iam-role.name
+  # Just for testing purpose, don't try to give administrator access
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
